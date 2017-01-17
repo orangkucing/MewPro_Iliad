ae02d5693a829695ee8215393deb00addd4d6807
orangkucing <orangkucing@mac.com>
2017-01-17 22:02:23 +0900

    video modes
    4K30, 720p120, WVGA

--- MenuText.h
+++ MenuText.h
@@ -1313,8 +1313,6 @@ if(id==1&&setting.b[53]==1)switch(v){case 0:case 1:case 2:case 3:return true;}
 if(id==27&&setting.b[53]==1)switch(v){case 0:case 1:case 2:return true;}
 if(id==1&&setting.b[53]==2)switch(v){case 0:case 1:case 2:case 3:return true;}
 if(id==16&&setting.b[53]==2)switch(v){case 0:case 1:case 2:return true;}
-// disable WVGA
-//if(id==2&&v==13)return true;
 return false;
 }
 
@@ -1456,8 +1454,6 @@ if(id==1&&setting.b[53]==1)switch(v){case 0:case 1:case 2:case 3:return true;}
 if(id==27&&setting.b[53]==1)switch(v){case 0:case 1:case 2:return true;}
 if(id==1&&setting.b[53]==2)switch(v){case 0:case 1:case 2:case 3:return true;}
 if(id==16&&setting.b[53]==2)switch(v){case 0:case 1:case 2:return true;}
-// disable WVGA
-//if(id==2&&v==13)return true;
 return false;
 }
 
